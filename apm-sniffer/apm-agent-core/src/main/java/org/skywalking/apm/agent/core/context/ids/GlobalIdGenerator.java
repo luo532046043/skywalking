9db53fd95e1c4c10f0f7a939e4484d7e2102ad3f
YunaiV <>
2017-12-29 17:54:27 +0800

    Agent 收集 Trace 数据。

--- apm-sniffer/apm-agent-core/src/main/java/org/skywalking/apm/agent/core/context/ids/GlobalIdGenerator.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/skywalking/apm/agent/core/context/ids/GlobalIdGenerator.java
@@ -18,15 +18,23 @@
 
 package org.skywalking.apm.agent.core.context.ids;
 
-import java.util.Random;
 import org.skywalking.apm.agent.core.conf.RemoteDownstreamConfig;
 import org.skywalking.apm.agent.core.dictionary.DictionaryUtil;
 
+import java.util.Random;
+
+/**
+ * 全局编号生成器
+ */
 public final class GlobalIdGenerator {
+
+    /**
+     * 线程变量，编号上下文
+     */
     private static final ThreadLocal<IDContext> THREAD_ID_SEQUENCE = new ThreadLocal<IDContext>() {
         @Override
         protected IDContext initialValue() {
-            return new IDContext(System.currentTimeMillis(), (short)0);
+            return new IDContext(System.currentTimeMillis(), (short) 0);
         }
     };
 
@@ -55,17 +63,27 @@ public final class GlobalIdGenerator {
         if (RemoteDownstreamConfig.Agent.APPLICATION_INSTANCE_ID == DictionaryUtil.nullValue()) {
             throw new IllegalStateException();
         }
+        // 获得 编号上下文
         IDContext context = THREAD_ID_SEQUENCE.get();
-
+        // 生成编号
         return new ID(
-            RemoteDownstreamConfig.Agent.APPLICATION_INSTANCE_ID,
-            Thread.currentThread().getId(),
-            context.nextSeq()
+            RemoteDownstreamConfig.Agent.APPLICATION_INSTANCE_ID, // 应用实例编号
+            Thread.currentThread().getId(), // 线程编号
+            context.nextSeq() // 带时间戳的序列号
         );
     }
 
+    /**
+     * 编号上下文
+     */
     private static class IDContext {
+        /**
+         * 最后生成编号时间
+         */
         private long lastTimestamp;
+        /**
+         * 线程自增序列号
+         */
         private short threadSeq;
 
         // Just for considering time-shift-back only.
@@ -78,34 +96,47 @@ public final class GlobalIdGenerator {
             this.threadSeq = threadSeq;
         }
 
+        /**
+         * @return 生成序列号
+         */
         private long nextSeq() {
             return timestamp() * 10000 + nextThreadSeq();
         }
 
+        /**
+         * @return 获得时间
+         */
         private long timestamp() {
             long currentTimeMillis = System.currentTimeMillis();
-
+            // 处理时间回退，使用随机数作为时间戳
             if (currentTimeMillis < lastTimestamp) {
                 // Just for considering time-shift-back by Ops or OS. @hanahmily 's suggestion.
                 if (random == null) {
                     random = new Random();
                 }
+                // 使用随机数作为时间戳。该判断用于，每毫秒，只随机生成一次随机数。
                 if (runRandomTimestamp != currentTimeMillis) {
                     lastRandomValue = random.nextInt();
                     runRandomTimestamp = currentTimeMillis;
                 }
                 return lastRandomValue;
+            // 当前时间
             } else {
                 lastTimestamp = currentTimeMillis;
                 return lastTimestamp;
             }
         }
 
+        /**
+         * @return 生成自增序列号
+         */
         private short nextThreadSeq() {
             if (threadSeq == 10000) {
                 threadSeq = 0;
             }
             return threadSeq++;
         }
+
     }
+
 }
